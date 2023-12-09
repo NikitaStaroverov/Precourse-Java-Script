3002df1c835e536702724a2fa2cab4f3f44abde1
NikitaStaroverov <starovierov98@mail.ru>
2023-12-09 17:25:38 +0300

    arrow function

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,1 +1,9 @@
-// code
+/**Переделай функцию на стрелочный синтаксис
+
+function doArrow(value) {
+    console.log(value);
+}
+doArrow('Передай 5') */
+
+const doArrow = value => console.log(value)
+doArrow('Передай 5')
